ab78dee7969d18172822fe665c95f732f0573d28
Poruri Sai Rahul <rporuri@enthought.com>
2018-07-25 13:23:07 +0530

    ADD : more paths
    the index page returns general info, the trains page provides a
    dummy list of trains that can be queried for and the train/* page
    will return a dummy response, with the train number

--- trains/__init__.py
+++ trains/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 
+TRAIN_LIST = ['12466', '18844', '12345']
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -24,10 +26,19 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    @app.route('/')
+    def home():
+        return 'see /trains for general info and /train/* for specific info'
 
     # a simple page that says hello
     @app.route('/trains')
     def trains():
-        return '10000'
-    
+        render_str = "Information about the following trains is available"
+        return render_str + f"{', '.join(TRAIN_LIST)}"
+
+    @app.route('/train/<train_no>')
+    def train(train_no):
+        print(train_no)
+        return f"{train_no}"
+
     return app
